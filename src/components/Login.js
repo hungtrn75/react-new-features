edec3227195a7186fa2c7664bcfa0071ce4857b7
Hung Tran <hungtrn75@gmail.com>
2018-10-29 14:29:19 +0700

    added static contextType

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import AuthContext from "../auth-context";
+
+class Login extends React.Component {
+  static contextType = AuthContext;
+  render() {
+    return (
+      <button onClick={this.context.toggleAuth}>
+        {this.context.isAuth ? "Logout" : "Login"}
+      </button>
+    );
+  }
+}
+
+export default Login;
